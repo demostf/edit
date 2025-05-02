48e94c7609754a6879adb9fb9e72f3f8bbd6b34e
Robin Appelman <robin@icewind.nl>
2025-05-02 19:24:41 +0200

    flake update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/release-22.11";
+    nixpkgs.url = "nixpkgs/release-24.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
